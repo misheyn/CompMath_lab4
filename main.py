fdeef88ed7f4402137cb8877936571c5ccefe727
misheyn <am.dolmatova01@gmail.com>
2021-11-14 14:19:11 +0700

    final version

--- main.py
+++ main.py
@@ -1,94 +1,50 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sympy
+from sympy.abc import p
+import math
 
 
-# Исходная функция
-def f(x):
+# исходная функция
+def function(x):
     return 5 * np.sin(x) - x + 1
 
 
-# 1 производная
-def f1(x):
-    return 5 * np.cos(x) - 1
+# функция для нахождения значения ошибок аппроксимации
+def mistake(a, b):
+    return math.fabs(a - b)
 
 
-# 2 производная
-def f2(x):
-    return -5 * np.sin(x)
+# полином Тейлора
+def taylor_polynomial(x, dec_p, n, func):
+    s = func.evalf(subs={p: dec_p})
+    func = func.diff(p)
+
+    for i in range(n + 1):
+        s += (func.evalf(subs={p: dec_p}) * ((x - dec_p) ** (i + 1))) / math.factorial(i + 1)
+        func = func.diff(p)
+
+    return s
 
 
-# 3 производная
-def f3(x):
-    return -5 * np.cos(x)
-
-
-# 4 производная
-def f4(x):
-    return 5 * np.sin(x)
-
-
-# Разложение Тейлора 1 степени
-def p1_x(x, c):
-    return f(c) + f1(c) * (x - c)
-
-
-# Разложение Тейлора 2 степени
-def p2_x(x, c):
-    return p1_x(x, c) + (f2(c) * (x - c) ** 2) / 2
-
-
-# Разложение Тейлора 3 степени
-def p3_x(x, c):
-    return p2_x(x, c) + (f3(c) * (x - c) ** 3) / 6
-
-
-# Разложение Тейлора 4 степени
-def p4_x(x, c):
-    return p3_x(x, c) + (f(c) * (x - c) ** 4) / 24
-
-
-# Ошибка 1 степени
-def r1_x(x, c):
-    return f(x) - p1_x(x, c)
-
-
-# Ошибка 2 степени
-def r2_x(x, c):
-    return f(x) - p2_x(x, c)
-
-
-# Ошибка 3 степени
-def r3_x(x, c):
-    return f(x) - p3_x(x, c)
-
-
-# Ошибка 4 степени
-def r4_x(x, c):
-    return f(x) - p4_x(x, c)
-
-
-# Нахождение ошибок аппроксимации
-def approximation_mistakes(a, b, st, dpoint):
-    m1 = np.zeros(round((b - a) / st))
-    m2 = np.zeros(round((b - a) / st))
-    m3 = np.zeros(round((b - a) / st))
-    m4 = np.zeros(round((b - a) / st))
-    i = 0
+# нахождение ошибок аппроксимации
+def approximation_mistakes(a, b, st, dec_p, func):
+    m1, m2, m3, m4 = [], [], [], []
     j = a
+
     while j < b - st:
-        m1[i] = r1_x(j, dpoint)
-        m2[i] = r2_x(j, dpoint)
-        m3[i] = r3_x(j, dpoint)
-        m4[i] = r4_x(j, dpoint)
+        m1.append(taylor_polynomial(j, dec_p, 1, func))
+        m2.append(taylor_polynomial(j, dec_p, 2, func))
+        m3.append(taylor_polynomial(j, dec_p, 3, func))
+        m4.append(taylor_polynomial(j, dec_p, 4, func))
         j += st
-        i += 1
+
     return m1, m2, m3, m4
 
 
-# Графики ошибок аппроксимации
-def draw_mistakes(x, m1, m2, m3, m4, dpoint):
-    plt.figure(figsize=(10, 10))
-    plt.title("Plots of approximation mistakes for the decomposition point x =" + str(dpoint))
+# графики ошибок аппроксимации
+def draw_mistakes(x, m1, m2, m3, m4, dec_p):
+    plt.title("Plots of approximation mistakes for the decomposition point x =" + str(dec_p))
     plt.plot(x, m1, label='Approximation mistake for a polynomial of degree 1')
     plt.plot(x, m2, label='Approximation mistake for a polynomial of degree 2')
     plt.plot(x, m3, label='Approximation mistake for a polynomial of degree 3')
@@ -101,23 +57,22 @@ def draw_mistakes(x, m1, m2, m3, m4, dpoint):
 
 
 # интерполяционный полином Лагранжа
-def lagrange_polynomial(x, new_p, func, n):
+def lagrange_polynomial(x, lag_p, func, n):
     s = 0
     for i in range(n):
         t = 1
         for j in range(n):
             if j != i:
-                t *= (x - new_p[j]) / (new_p[i] - new_p[j])
-        s += (func(new_p[i]) * t)
+                t *= (x - lag_p[j]) / (lag_p[i] - lag_p[j])
+        s += (func.evalf(subs={p: lag_p[i]}) * t)
     return s
 
 
 # графики интерполяционного полинома Лагранжа
 def draw_lagrange_poly(x, func, lag_p, n):
-    plt.figure(figsize=(10, 10))
     plt.title("The graphs of the original function and the Lagrange polynomial for n =" + str(n))
     plt.plot(x, lagrange_polynomial(x, lag_p, func, n), 'g--')
-    plt.plot(x, func(x), color="red")
+    plt.plot(x, function(x), color="red")
     plt.axhline(y=0, color="black", linewidth=1.5)
     plt.axvline(x=0, color="black", linewidth=1.5)
     plt.grid(True)
@@ -159,23 +114,44 @@ def draw_sliding_poly(points, b0, b1, b2):
     plt.show()
 
 
-A = -7.0
-B = 9.0
-ST = 0.002
+A = -3.5
+B = 4.7
+ST = 0.01
 X = np.arange(A, B, ST)
-dec_points = [-7, 9, 1]
-lag_points1 = [-7, -4, 0, 3, 8]
-lag_points2 = [-7, -5, -2, -1, 0, 2, 3, 5, 7, 8]
+xn = 0.9
+dec_points = [-3.5, 4.7, 0.6]
+lag_points1 = [-3.2, -1.8, 0.8, 2.5, 3.9]
+lag_points2 = [-3.3, -2.6, -1.1, -0.3, 0.7, 1.1, 2.6, 2.9, 3.4, 4.6]
+f = 5 * sympy.sin(p) - p + 1
+y = f.evalf(subs={p: xn})
 
+# Исследование ошибок аппроксимации многочленом Тейлора
 for i in range(len(dec_points)):
-    app_m1, app_m2, app_m3, app_m4 = approximation_mistakes(A, B, ST, dec_points[i])
+    app_m1, app_m2, app_m3, app_m4 = approximation_mistakes(A, B, ST, dec_points[i], f)
     draw_mistakes(X, app_m1, app_m2, app_m3, app_m4, dec_points[i])
 
+for n in range(1, 5):
+    print("\nFor n =", n, "mistakes:")
+    print("c = a: %.7f" % mistake(y, taylor_polynomial(xn, dec_points[0], n, f)))
+    print("c = b: %.7f" % mistake(y, taylor_polynomial(xn, dec_points[1], n, f)))
+    print("c = (a+b)/2: %.7f" % mistake(y, taylor_polynomial(xn, dec_points[2], n, f)))
+
+# Исследование ошибок интерполяции многочленом Лагранжа
+print("\nLagrange, n = 5")
 draw_lagrange_poly(X, f, lag_points1, 5)
+y1 = lagrange_polynomial(xn, lag_points1, f, 5)
+for n in range(1, 5):
+    print("For degree n =", n)
+    print("mistake = %.7f\n" % mistake(y1, taylor_polynomial(xn, dec_points[2], n, f)))
+
+print("\nLagrange, n = 10")
 draw_lagrange_poly(X, f, lag_points2, 10)
+y2 = lagrange_polynomial(xn, lag_points2, f, 10)
+for n in range(1, 5):
+    print("For degree n =", n)
+    print("mistake = %.7f\n" % mistake(taylor_polynomial(xn, dec_points[2], n, f), y2))
 
+# Исследование влияние степени на ошибку интерполирования "скользящими" полиномами
 for i in range(2, 16, 1):
-    sliding_points, a0, a1, a2 = sliding_filling(A, f, i)
+    sliding_points, a0, a1, a2 = sliding_filling(A, function, i)
     draw_sliding_poly(sliding_points, a0, a1, a2)
-
-print(r1_x(X, dec_points[0]))
